ed50a567b257eebc1aeaa04115eb2a6a3f62a30e
ephson <727468+philote@users.noreply.github.com>
2024-03-31 03:03:09 -0600

    added pbta sheet code

---
+++ scripts/helpers/config-sheet.mjs
@@ -0,0 +1,88 @@
+export const configSheet = async () => {
+    // Pass Masks sheet object to sheetConfig
+    game.pbta.sheetConfig = {
+        rollFormula: "2d6",
+        statToggle: {
+            label: game.i18n.localize("PBTA-TEMPLATE.CharacterSheets.statToggle"),
+            modifier: 0
+        },
+        minMod: -3,
+        maxMod: 4,
+        rollResults: {
+            failure: {
+                start: null,
+                end: 6,
+                label: game.i18n.localize("PBTA-TEMPLATE.CharacterSheets.rollResults.complications")
+            },
+            partial: {
+                start: 7,
+                end: 9,
+                label: game.i18n.localize("PBTA-TEMPLATE.CharacterSheets.rollResults.partialSuccess")
+            },
+            success: {
+                start: 10,
+                end: null,
+                label: game.i18n.localize("PBTA-TEMPLATE.CharacterSheets.rollResults.success")
+            }
+        },
+        actorTypes: {
+            character: {
+                stats: {
+                    one: {
+                        label: game.i18n.localize("PBTA-TEMPLATE.CharacterSheets.stats.one"),
+                        value: 0
+                    },
+                    two: {
+                        label: game.i18n.localize("PBTA-TEMPLATE.CharacterSheets.stats.two"),
+                        value: 0
+                    },
+                    three: {
+                        label: game.i18n.localize("PBTA-TEMPLATE.CharacterSheets.stats.three"),
+                        value: 0
+                    }
+                },
+                attrTop: {
+                    
+                },
+                attrLeft: {
+                    
+                },
+                details: {
+                    biography: {
+                        label: game.i18n.localize("PBTA-TEMPLATE.CharacterSheets.details.biographyLabel"),
+                        value: ""
+                    }
+                },
+                moveTypes: {
+                    basic: {
+                        label: game.i18n.localize("PBTA-TEMPLATE.CharacterSheets.moveTypes.basicLabel"),
+                        moves: [],
+                        creation: true
+                    },
+                    playbook: {
+                        label: game.i18n.localize("PBTA-TEMPLATE.CharacterSheets.moveTypes.playbookLabel"),
+                        moves: [],
+                        playbook: true
+                    }
+                }
+            },
+            npc: {
+                attrLeft: {
+                    
+                },
+                details: {
+                    biography: {
+                        label: game.i18n.localize("PBTA-TEMPLATE.NPCSheets.details.biographyLabel"),
+                        value: ""
+                    }
+                },
+                moveTypes: {
+                    villain: {
+                        label: game.i18n.localize("PBTA-TEMPLATE.NPCSheets.moveTypes.basicLabel"),
+                        moves: []
+                    }
+                }
+            }
+        }
+    }
+};
---
+++ scripts/pbta-template.mjs
@@ -0,0 +1,17 @@
+import { configSheet } from "./helpers/config-sheet.mjs";
+
+Hooks.once('pbtaSheetConfig', () => {
+    // Disable the sheet config form.
+    game.settings.set('pbta', 'sheetConfigOverride', true);
+
+    // Replace the game.pbta.sheetConfig with your own version.
+    configSheet();
+
+    // PBTA Settings
+    game.settings.set('pbta', 'advForward', true);
+    game.settings.set('pbta', 'hideRollFormula', true);
+    game.settings.set('pbta', 'hideForward', false);
+    game.settings.set('pbta', 'hideOngoing', false);
+    game.settings.set('pbta', 'hideRollMode', false);
+    game.settings.set('pbta', 'hideUses', true);
+});
